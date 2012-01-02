59ea0f2fbaa0b6f74613fa9e946f9c188604960b
Duke <duke@riseup.net>
2012-01-02 21:00:11 -0200

    fixed bug with safari run before load

--- javascripts/enhancements.js
+++ javascripts/enhancements.js
@@ -3,23 +3,25 @@
 // https://github.com/benschwarz/w3c-spec-styles/blob/master/userscript.js
 //
 var W3C = function(){
-  var elements = {
-    link: {
-      href: "http://vertigem.xxx/w3c-spec-styles/docs/css/master.css",
-      rel:  "stylesheet"
-    },
-    script: {
-      src: "http://vertigem.xxx/w3c-spec-styles/docs/js/enhancements.js",
-      async: true
+  if(document.body){
+    var elements = {
+      link: {
+        href: "http://vertigem.xxx/w3c-spec-styles/docs/css/master.css",
+        rel:  "stylesheet"
+      },
+      script: {
+        src: "http://vertigem.xxx/w3c-spec-styles/docs/js/enhancements.js",
+        async: true
+      }
+    }
+
+    for(var tag in elements) {
+      var element = document.createElement(tag);
+      for(var attribute in elements[tag]) {
+        element.setAttribute(attribute, elements[tag][attribute]);
+        document.body.appendChild(element);
+      }
     }
   }
-
-  for(var tag in elements) {
-		var element = document.createElement(tag);
-		for(var attribute in elements[tag]) {
-			element.setAttribute(attribute, elements[tag][attribute]);
-			document.body.appendChild(element);
-		}
-	}
 }();
 
